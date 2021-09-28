50bf59ff9bc1141ea4786a88cc299005c5f3e5cc
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-28 16:07:26 +0300

    chan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-      Edit me help!
-      </p>
-    </div>
+    <>
+      <Router></Router>
+    </>
   );
 }
 
