274a3a0a6850ad5af051236152d164f3e179c63b
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-28 16:24:15 +0300

    changes

--- src/pages/topbottombar/navbar.js
+++ src/pages/topbottombar/navbar.js
@@ -1,1 +1,12 @@
-hello
+import React from "react";
+import "./navbar.js";
+
+function Navbar(){
+return(
+    <>
+        <div><ul><li>Home</li><li>Programs</li><li>Account</li></ul></div>
+    </>
+)
+}
+
+export default Navbar;
