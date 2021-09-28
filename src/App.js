75ef98f9292b7782490df5e6ea8b812a65cb3c5c
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-28 16:26:10 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import './App.css';
+import Navbar from "./pages/topbottombar/navbar";
 
 function App() {
   return (
     <>
-      <Router></Router>
+      <Router>
+        <Navbar/>
+      </Router>
     </>
   );
 }
